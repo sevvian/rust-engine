e7314cab387a5492e5ed4b2e0215699bc9858ce5
sevvian <59226487+sevvian@users.noreply.github.com>
2026-04-05 22:28:08 -0700

    Update build.gradle.kts

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -1,11 +1,14 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+//--- START OF FILE rust-app-main/android-app/build.gradle.kts ---
+// Top-level build file for 2026 Rust-Android Stack
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("org.mozilla.rust-android-gradle.rust-android") version "0.9.3" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.mozilla.rust-android-gradle.rust-android") version "0.9.6" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
 
 tasks.register<Delete>("clean") {
     delete(layout.buildDirectory)
 }
+//--- END OF FILE rust-app-main/android-app/build.gradle.kts ---
